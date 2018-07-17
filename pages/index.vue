abef0edd4236f834059025c7106684a708a7cb77
yagisuke <yagisuke.0209@gmail.com>
2018-07-18 01:31:05 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <Title>weekly2018</Title>
+    <p>これからつくる.</p>
+  </section>
+</template>
+
+<script>
+import styled from 'vue-styled-components'
+
+const Title = styled.h1`
+  font-weight: 400;
+  font-size: 24px;
+`;
+
+export default {
+  components: {
+    Title
+  }
+}
+</script>
