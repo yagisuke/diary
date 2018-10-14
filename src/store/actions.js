18ec0bd82a1084d8174fabe127c4d97d7930d1b4
yagisuke <yagisuke.0209@gmail.com>
2018-10-14 17:32:16 +0900

    create store file

---
+++ src/store/actions.js
@@ -0,0 +1,11 @@
+import * as types from './mutation-type'
+import { Auth, List, Task } from '../api'
+
+export default {
+  login: ({ commit }) => {},
+  fetchLists: ({ commit }) => {},
+  addTask: ({ commit }) => {},
+  updateTask: ({ commit }) => {},
+  removeTask: ({ commit }) => {},
+  logout: ({ commit }) => {}
+}
---
+++ src/store/mutations.js
@@ -0,0 +1,10 @@
+import * as types from './mutation-types'
+
+export default {
+  [types.AUTH_LOGIN] (state, payload) {},
+  [types.FETCH_ALL_TASKLIST] (state, payload) {},
+  [types.ADD_TASK] (state, payload) {},
+  [types.UPDATE_TASK] (state, payload) {},
+  [types.REMOVE_TASK] (state, payload) {},
+  [types.AUTH_LOGOUT] (state, payload) {}
+}
