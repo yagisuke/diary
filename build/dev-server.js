35431333587734e504d319f6fd9b65708d2a59fa
yagisuke <yagisuke.0209@gmail.com>
2018-10-14 15:27:26 +0900

    create store

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,5 +1,7 @@
 const bodyParser = require('body-parser')
 
+// Express アプリケーションインスタンスを受け取る関数をエクスポート
 module.exports = app => {
+  // HTTPリクエストのbodyの内容をJSONとして解析するようミドルウェアをインストール
   app.use(bodyParser.json())
 }
