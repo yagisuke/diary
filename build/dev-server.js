1813e6f51f3b2e30d7ca3db2178cab76f33c7193
yagisuke <yagisuke.0209@gmail.com>
2018-10-20 23:27:05 +0900

    mock

--- build/dev-server.js
+++ build/dev-server.js
@@ -4,4 +4,27 @@ const bodyParser = require('body-parser')
 module.exports = app => {
   // HTTPリクエストのbodyの内容をJSONとして解析するようミドルウェアをインストール
   app.use(bodyParser.json())
+
+  const users = {
+    'hoge@hoge.com': {
+      password: 'password',
+      userId: 1,
+      token: '123456'
+    }
+  }
+
+  app.post('/auth/login', (req, res) => {
+    const { email, password } = req.body
+    const user = users[email]
+
+    if (user) {
+      if (user.password !== password) {
+        res.status(401).json({ message: 'メールアドレス又はパスワードが間違っています。' })
+      } else {
+        res.json({ userId: user.userId, token: user.token })
+      }
+    } else {
+      res.status(404).json({ message: 'ユーザーが登録されていません' })
+    }
+  })
 }
